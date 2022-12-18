953b80e359450333c9ab79dee70a609c76202faf
sara <sara20191701099@cis.asu.edu.eg>
2022-12-18 18:21:48 +0200

    - pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:idental/layout/home_screen.dart';
+import 'package:idental/screens/appointment_screen.dart';
 
 import './screens/getting_started_screen.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: GettingStartedScreen(),
+      home: AppointmentScreen(),
     );
   }
 }
